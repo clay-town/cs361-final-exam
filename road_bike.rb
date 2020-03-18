793a20dbe43b7b6fdd57223343f018f27b9d39b9
clay-town <justinmccune@icloud.com>
2020-03-18 11:53:36 -0700

    add hourly rate to road bike

--- road_bike.rb
+++ road_bike.rb
@@ -2,11 +2,12 @@ require_relative 'pannier'
 
 class RoadBike
 
-  attr_accessor :weekly_rate, :daily_rate
+  attr_accessor :weekly_rate, :daily_rate, :hourly_rate
 
   def initialize
     @daily_rate = 15
     @weekly_rate = 105
+    @hourly_rate = 5
   end
 
   def clean
