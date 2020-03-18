2000bc0e15152e52659b205917bd6f72069b961b
clay-town <justinmccune@icloud.com>
2020-03-18 11:46:59 -0700

    remove white space and comments from panier

--- pannier.rb
+++ pannier.rb
@@ -5,26 +5,7 @@ class Pannier
   attr_accessor :price
 
   def initialize
-    @price = 3 # Set price. Flat rate per rental
+    @price = 3
   end
 
-  end
-
-
-
-  # def weight
-  #   @weight
-  # end
-
-
-
-
-
-
-
-
-
-
-
-
-
+end
